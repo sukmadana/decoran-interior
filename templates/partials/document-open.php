2caa9f0a309a3b0912caa5957a5d1670d60160d9
Sukmadana <sukmadana2212@gmail.com>
2023-11-03 16:12:55 +0800

    adding favicon

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -6,6 +6,7 @@
 		      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title><?= wp_get_document_title() ?></title>
+		<link rel="shortcut icon" href="<?=get_template_directory_uri()?>/assets/img/favicon.ico" type="image/x-icon">
 		<script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');</script>
 		<?php wp_head() ?>
 	</head>
